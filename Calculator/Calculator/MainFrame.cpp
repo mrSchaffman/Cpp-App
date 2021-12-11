2e126dad9cad9015bb07c27a6b553153c2605212
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2021-12-11 09:36:10 +0100

    Update

--- Calculator/Calculator/MainFrame.cpp
+++ Calculator/Calculator/MainFrame.cpp
@@ -5,81 +5,85 @@ END_EVENT_TABLE()
 MainFrame::MainFrame(const wxString&title):wxFrame(NULL,wxID_ANY,title,wxPoint(300,300),wxSize(X,Y))
 {
 	SetMaxSize(wxSize(X, Y));
-	labels = { "EXP","hyp","->","y^x","sin","a","Rac","cos","b","DEG","tan","x^2","Ln","(","F-E","log",")","x-M" };
-	labels2 = { "7","4","1","0","8","5","2",".","9","6","3","+/-","/","X","-","+" };
+	
 	topSizer = new wxBoxSizer(wxVERTICAL);
 
-	//SetFrameSizer(topSizer);
-	//SetDisplay(display);
-	//SetBoxSizer(firstSizer, wxHORIZONTAL);
-	display = new wxTextCtrl(this, 1001, wxEmptyString, wxPoint(10, 10), wxSize(250, 40), wxTE_RIGHT | wxTE_WORDWRAP);
-	display->SetEditable(false);
-	display->SetFont(GetFont().Scale(2));
-	
-	
-	firstSizer = new wxBoxSizer(wxHORIZONTAL);
-	wxButton *btnCE = new wxButton(this, 1002, "CE", wxDefaultPosition, wxSize(40, 40));
-	wxButton *btnDGR = new wxButton(this, 1003, "DGR", wxDefaultPosition, wxSize(40, 40));
-	wxButton *btn2nd = new wxButton(this, 1004, "2nd", wxDefaultPosition, wxSize(60, 40));
-	wxButton *btnOFF = new wxButton(this, 1005, "OFF", wxDefaultPosition, wxSize(40, 40));
-	wxButton *btnON = new wxButton(this, 1006, "ON", wxDefaultPosition, wxSize(40, 40));
-	firstSizer->Add(btnCE, 0,   wxALL,5);
-	firstSizer->Add(btnDGR, 0,  wxALL,5);
-	firstSizer->Add(btn2nd, 0, wxALL, 5);
-	firstSizer->Add(btnOFF, 0,  wxALL,5);
-	firstSizer->Add(btnON, 0,  wxALL,5);
-	firstSizer->Fit(this);
+	line0 = new wxBoxSizer(wxVERTICAL);
+	line1 = new wxBoxSizer(wxHORIZONTAL);
 
-	btns = new wxButton*[row1*column1];
-	secondSizer = new wxGridSizer(3, 6, 5, 5);
+	line2 = new wxGridSizer(rowL2, columnL2, 5, 5);
+	btnsLine2 = new wxButton*[rowL2*columnL2];
 
-	
-	
-		for (size_t x = 0; x < row1; x++)
+	line3 = new wxBoxSizer(wxHORIZONTAL);
+	line3_1 = new wxGridSizer(rowL3, columnL3, 5, 5);
+	btnsLine3_1 = new wxButton*[rowL3*columnL3];
+	line3_2 = new wxBoxSizer(wxVERTICAL);
+
+	frameSizers = { line0 ,line1,line2,line3 };
+
+	labelsLine2 = { "EXP","hyp","->","y^x","sin","a","Rac","cos","b","DEG","tan","x^2","Ln","(","F-E","log",")","x-M" };
+	labelsLine3_1 = { "7","4","1","0","8","5","2",".","9","6","3","+/-","/","*","-","+" };
+	labelsLine3_2 = { "MR","M+","="};
+
+
+	for (size_t line = 0; line <= 3; line++)
+	{
+		if (line == 0)
 		{
-			for (size_t y = 0; y < column1; y++)
-			{
-
-				btns[y*row1 + x] = new wxButton(this, 1007 + (y*row1 + x), labels[y*row1 + x], wxDefaultPosition, wxSize(50, 35));
-				btns[y*row1 + x]->SetFont(GetFont().Scale(1.5));
-				secondSizer->Add(btns[y*row1 + x], 0, wxEXPAND | wxALL);
-
-
-			}
+			display = new wxTextCtrl(this, 1000, wxEmptyString, wxDefaultPosition, wxSize(300, 40), wxTE_RIGHT || wxTE_WORDWRAP);
+			display->SetEditable(false);
+			display->SetFont(GetFont().Scale(1.5));
+		
+			frameSizers[line]->Add(display, 0, wxALL,5);
 		}
-	
-		thirdSizer = new wxBoxSizer(wxHORIZONTAL);
-		wxGridSizer * grid2 = new wxGridSizer(4, 4, 5, 5);
-		for (size_t x = 0; x < row2; x++)
+		if (line == 1)
 		{
-			for (size_t y = 0; y < column2; y++)
-			{
 
-				btns[y*row2 + x] = new wxButton(this, 1007 + (y*row2 + x), labels2[y*row2 + x], wxDefaultPosition, wxSize(50, 50));
-				btns[y*row2 + x]->SetFont(GetFont().Scale(2));
-				grid2->Add(btns[y*row2 + x], 0, wxEXPAND | wxALL);
-
-			}
 		}
-		wxBoxSizer * box = new wxBoxSizer(wxVERTICAL);
-		wxButton *btnMR = new wxButton(this, 1002, "MR", wxDefaultPosition, wxSize(50, 50));
-		wxButton *btnMplus = new wxButton(this, 1002, "M+", wxDefaultPosition, wxSize(50, 50));
-		wxButton *btnEqual = new wxButton(this, 1002, "=", wxDefaultPosition, wxSize(50, 90));
-		btnMR->SetFont(GetFont().Scale(2));
-		btnMplus->SetFont(GetFont().Scale(2));
-		btnEqual->SetFont(GetFont().Scale(2));
+		if (line == 2)
+		{
+			/*for (size_t x = 0; x < rowL2; x++)
+			{
+				for (size_t y = 0; y < columnL2; y++)
+				{
+					btnsLine2[y*rowL2 + x] = new wxButton(this, 2000 + (y*rowL2 + x), labelsLine2[y*rowL2 + x], wxDefaultPosition, wxSize(30, 30));
+					btnsLine2[y*rowL2 + x]->SetFont(GetFont().Scale(1.3));
+					line2->Add(btnsLine2[y*rowL2 + x], 0, wxALL);
+				}
+			}
 
-		box->Add(btnMR, 0, wxBOTTOM,5);
-		box->Add(btnMplus, 0, wxBOTTOM, 5);
-		box->Add(btnEqual, 0, wxALL);
-
-		thirdSizer->Add(grid2, 0, wxALL);
-		thirdSizer->Add(box, 0, wxLEFT, 5);
-
-	topSizer->Add(display, 0, wxALL | wxEXPAND, 10);
-	topSizer->Add(firstSizer, 0, wxALIGN_CENTER| wxALL, 5 );
-	topSizer->Add(secondSizer, 0, wxALIGN_CENTER|wxALL,5);
-	topSizer->Add(thirdSizer, 0, wxALIGN_CENTER| wxALL,5 );
+			frameSizers[line]->Add(line2, 0, wxALIGN_CENTER);*/
+		}
+		if (line == 3)
+		{
+			for (size_t x = 0; x < rowL3; x++)
+			{
+				for (size_t y = 0; y < columnL3; y++)
+				{
+					btnsLine3_1[y*rowL3 + x] = new wxButton(this, 3010 + (y*rowL3 + x), labelsLine3_1[y*rowL3 + x], wxDefaultPosition, wxSize(50, 50));
+					btnsLine3_1[y*rowL3 + x]->SetFont(GetFont().Scale(2));
+					line3_1->Add(btnsLine3_1[y*rowL3 + x], 0, wxALL);
+					btnsLine3_1[y*rowL3 + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &MainFrame::OnButtonNumberClick, this);
+				}
+			}
+			frameSizers[line]->Add(line3_1, 0, wxALL);
+			for (size_t line32 = 0; line32 < 2; line32++)
+			{
+				wxButton *btn32 = new wxButton(this, 3020 + line32, labelsLine3_2[line32],wxDefaultPosition,wxSize(50,50));
+				btn32->SetFont(GetFont().Scale(2));
+				line3_2->Add(btn32, 0, wxBOTTOM,5);
+				btn32->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &MainFrame::OnButtonOperatorClick, this); // Handler to modify.
+			}
+			// the Equal button
+			wxButton *btn32 = new wxButton(this, 3022, labelsLine3_2[2], wxDefaultPosition, wxSize(50, 105));
+			btn32->SetFont(GetFont().Scale(2));
+			line3_2->Add(btn32, 0, wxBOTTOM, 5);
+			btn32->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &MainFrame::OnButtonOperatorClick, this);
+			frameSizers[line]->Add(line3_2, 0,wxLEFT,5);
+		}
+		
+		topSizer->Add(frameSizers[line], 0, wxALIGN_CENTER);
+	}
 	topSizer->Fit(this);
 	topSizer->SetSizeHints(this);
 	SetSizer(topSizer);
@@ -88,30 +92,17 @@ MainFrame::~MainFrame()
 {
 
 }
-void MainFrame::SetFrameSizer(wxBoxSizer * sizer)
+	
+
+void MainFrame::OnButtonNumberClick(wxCommandEvent &evt)
 {
-	
-	sizer = new wxBoxSizer(wxVERTICAL);
-	
+
+
+	display->AppendText(wxString("${}",evt.GetId()));
 }
-void MainFrame::SetDisplay(wxTextCtrl * display)
+void MainFrame::OnButtonOperatorClick(wxCommandEvent &evt)
 {
-	display = new wxTextCtrl(this, 1001, wxEmptyString, wxPoint(10,10), wxSize(300, 40), wxTE_RIGHT | wxTE_WORDWRAP);
-	display->SetEditable(false);
-	display->SetFont(GetFont().Scale(1.5));
-}
-void MainFrame::SetBoxSizer(wxBoxSizer * sizer, wxOrientation orientation)
-{
-	sizer = new wxBoxSizer(orientation);
-	wxButton *btnCE = new wxButton(this, 1002, "CE", wxDefaultPosition, wxSize(40, 40));
-	wxButton *btnDGR = new wxButton(this, 1003, "DGR", wxDefaultPosition, wxSize(40, 40));
-	wxButton *btn2nd = new wxButton(this, 1004, "2nd", wxDefaultPosition, wxSize(50, 40));
-	wxButton *btnOFF = new wxButton(this, 1005, "OFF", wxDefaultPosition, wxSize(40, 40));
-	wxButton *btnON = new wxButton(this, 1006, "ON", wxDefaultPosition, wxSize(40, 40));
-	sizer->Add(btnCE,0,wxEXPAND|wxALL,0);
-	sizer->Add(btnDGR,0,wxEXPAND|wxALL,0);
-	sizer->Add(btn2nd,0,wxEXPAND|wxALL,0);
-	sizer->Add(btnOFF,0,wxEXPAND|wxALL,0);
-	sizer->Add(btnON,0,wxEXPAND|wxALL,0);
-	
+
+
+	display->AppendText(wxString("t",evt.GetId()));
 }
--- Calculator/Calculator/MainFrame.h
+++ Calculator/Calculator/MainFrame.h
@@ -2,6 +2,10 @@
 #include"wx/wx.h"
 static const size_t X = 300;
 static const size_t Y = 500;
+static const size_t rowL2 = 4;
+static const size_t columnL2 = 7;
+static const size_t rowL3 = 4;
+static const size_t columnL3 = 4;
 
 class MainFrame : public wxFrame
 {
@@ -10,23 +14,31 @@ public:
 	~MainFrame();
 
 private:
-	size_t row1 = 3;
-	size_t column1 = 6;
-	size_t row2 = 4;
-	size_t column2 = 4;
-	wxButton **btns = nullptr;
-	wxBoxSizer * topSizer = nullptr;
+	wxBoxSizer *topSizer = nullptr;
+
 	wxTextCtrl * display = nullptr;
 
-	wxBoxSizer * firstSizer = nullptr;
-	wxGridSizer * secondSizer = nullptr;
-	wxBoxSizer * thirdSizer = nullptr;
-	std::vector<wxString> labels;
-	std::vector<wxString> labels2;
+	wxBoxSizer * line0 = nullptr;
 
-	void SetFrameSizer(wxBoxSizer * sizer);
-	void SetDisplay(wxTextCtrl * display);
-	void SetBoxSizer(wxBoxSizer * sizer, wxOrientation orientation);
-	DECLARE_EVENT_TABLE()
-};
+	wxBoxSizer * line1 = nullptr;
 
+	wxGridSizer * line2 = nullptr;
+	wxButton** btnsLine2 = nullptr;
+
+	wxBoxSizer * line3 = nullptr;
+	wxGridSizer * line3_1 = nullptr;
+	wxButton** btnsLine3_1 = nullptr;
+	wxBoxSizer * line3_2 = nullptr;
+
+	std::vector<wxSizer*>frameSizers;
+
+	std::vector<wxString> labelsLine2;
+	std::vector<wxString> labelsLine3_1;
+	std::vector<wxString> labelsLine3_2;
+
+	void OnButtonOperatorClick(wxCommandEvent &evt);
+	void OnButtonNumberClick(wxCommandEvent &evt);
+
+	std::vector<wxOrientation>orientation;
+		DECLARE_EVENT_TABLE()
+	};	
