4317852afc91691dcc85d6ee23fc293840de0eaf
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2022-01-30 21:00:11 +0100

    updte

--- P-15-Others/NotePad/App/Card.cpp
+++ P-15-Others/NotePad/App/Card.cpp
@@ -23,18 +23,19 @@ void Card::OnPaint(wxPaintEvent & event)
 
 void Card::OnMouse(wxMouseEvent & event)
 {
-	this->ClearBackground();
 
 	wxClientDC dc(this);
 	PrepareDC(dc);
 
 	if (event.Entering())
 	{
+		this->ClearBackground();
 		SetCardSize(wxSize(m_size.GetWidth() + 4,m_size.GetHeight() + 4));
 		DrawCard(dc);
 	}
 	if (event.Leaving())
 	{
+		this->ClearBackground();
 		SetCardSize(wxSize(m_size.GetWidth(), m_size.GetHeight()));
 		DrawCard(dc);
 	}
