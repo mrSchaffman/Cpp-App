5e33606a5b47cfd46e751f2058ebf8f4d0797cda
Barth. Feudong <61944369+mrSchaffman@users.noreply.github.com>
2022-03-28 09:33:53 +0200

    implementation of the OnQuit Methode

--- P-16-Samples/Access/Access_v9/MainFrame.cpp
+++ P-16-Samples/Access/Access_v9/MainFrame.cpp
@@ -1,12 +1,14 @@
 #include "MainFrame.h"
 wxBEGIN_EVENT_TABLE(MainFrame,wxFrame)
-
+	EVT_MENU(ID_ABOUT,MainFrame::OnAbout)
+	EVT_MENU(ID_QUERY,MainFrame::OnQuery)
+	EVT_MENU(ID_QUIT,MainFrame::OnQuit)
 wxEND_EVENT_TABLE()
 
 MainFrame::MainFrame(const wxString & caption, const wxPoint & pos, const wxSize & size, long style)
 	: wxFrame(nullptr,wxID_ANY, caption, pos,size,style)
 {
-
+	LayoutControls();
 }
 
 void MainFrame::Init()
@@ -19,4 +21,52 @@ bool MainFrame::Create(const wxString & caption, const wxPoint & pos, const wxSi
 	Init();
 	MainFrame(caption, pos, size, style);
 	return true;
+}
+
+void MainFrame::LayoutControls()
+{
+#if wxUSE_MENUS
+
+	wxMenu* menuFile = new wxMenu;
+	{
+		menuFile->Append(ID_QUERY, wxT("Query"), wxT("Query the Window hierachy"));
+		menuFile->AppendSeparator();
+		menuFile->Append(ID_QUIT, wxT("E&xit\tAlt-F4"), wxT("Exit the Program"));
+
+	}
+	wxMenu* menuHelp = new wxMenu;
+	{
+		menuHelp->Append(ID_ABOUT,wxT("&About"),wxT("Show the About dialog"));
+	}
+
+	wxMenuBar* menuBar = new wxMenuBar();
+	{
+		menuBar->Append(menuFile, wxT("&File"));
+		menuBar->Append(menuHelp, wxT("&Help"));
+	}
+
+	SetMenuBar(menuBar);
+#endif // wxUSE_MENUS
+
+}
+
+void MainFrame::OnAbout(wxCommandEvent& event)
+{
+}
+
+void MainFrame::OnQuery(wxCommandEvent& event)
+{
+}
+
+void MainFrame::OnQuit(wxCommandEvent& event)
+{
+	Close(this);
+}
+
+void MainFrame::Log(const wxString& message)
+{
+}
+
+void MainFrame::LogObject(int intent, IAccessible* object)
+{
 }
--- P-16-Samples/Access/Access_v9/MainFrame.h
+++ P-16-Samples/Access/Access_v9/MainFrame.h
@@ -1,5 +1,11 @@
 #pragma once
 #include"wx/wx.h"
+enum MenuID
+{
+	ID_ABOUT = 10,
+	ID_QUERY,
+	ID_QUIT,
+};
 class MainFrame : public wxFrame
 {
 public:
@@ -15,10 +21,21 @@ public:
 		const wxSize& size = wxDefaultSize,
 		long style = wxDEFAULT_FRAME_STYLE);
 
+	void LayoutControls();
 
+	// event handlers
+	void OnAbout(wxCommandEvent& event);
+	void OnQuery(wxCommandEvent& event);
+	void OnQuit(wxCommandEvent& event);
+
+	void Log(const wxString&message);
+	void LogObject(int intent, IAccessible* object);
+
+	// void GetInfo(IAccessible* accessible,int id, wxString& name, wxString&role);
 
 private:
 
+	wxTextCtrl* m_logger;
 	wxDECLARE_EVENT_TABLE();
 };
 
