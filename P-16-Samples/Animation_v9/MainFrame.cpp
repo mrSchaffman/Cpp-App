e973593cbd0040d38eb90ae58e7dc3fcc26468ae
Barth. Feudong <61944369+mrSchaffman@users.noreply.github.com>
2022-03-29 13:02:15 +0200

    The Definition of the MainFrame

--- P-16-Samples/Animation_v9/MainFrame.cpp
+++ P-16-Samples/Animation_v9/MainFrame.cpp
@@ -4,6 +4,12 @@ wxBEGIN_EVENT_TABLE(MainFrame,wxFrame)
 
 wxEND_EVENT_TABLE()
 
-MainFrame::MainFrame(const wxString& caption):wxFrame(nullptr,wxID_ANY,caption,wxDefaultPosition,wxSize(370,220))
+MainFrame::MainFrame(wxWindow* parent,
+	const wxWindowID id,
+	const wxString& caption,
+	const wxPoint& pos,
+	const wxSize& size,
+	const long style):wxFrame(nullptr,wxID_ANY,caption,wxDefaultPosition,wxSize(370,220))
 {
+
 }
--- P-16-Samples/Animation_v9/MainFrame.h
+++ P-16-Samples/Animation_v9/MainFrame.h
@@ -1,14 +1,46 @@
 #pragma once
 #include"wx/wx.h"
+#include"wx/animate.h"
+
 #include"MathChallenge.h"
 class MainFrame : public wxFrame
 {
 public:
-	MainFrame(const wxString& caption);
-	virtual ~MainFrame() = default;
+	MainFrame(wxWindow* parent, 
+		const wxWindowID id, 
+		const wxString& caption,
+		const wxPoint& pos, 
+		const wxSize& size, 
+		const long style);
+
+	 ~MainFrame();
+
+	 // event Handlers
+	 void OnAbout(wxCommandEvent& event);
+	 void OnQuit(wxCommandEvent& event);
+	 void OnPlay(wxCommandEvent& event);
+	 void OnSetNullAnimation(wxCommandEvent& event);
+	 void OnSetInactiveBitmap(wxCommandEvent& event);
+	 void OnSetNoAutoResize(wxCommandEvent& event);
+	 void OnSetBgColor(wxCommandEvent& event);
+	 void OnStop(wxCommandEvent& event);
+
+	 void OnUpdateUI(wxUpdateUIEvent& event);
+
+#ifdef wxHAS_NATIVE_ANIMATIONCTRL
+	 void OnUseGeneric(wxCommandEvent& event);
+#endif // wxHAS_NATIVE_ANIMATIONCTRL
+
+#if wxUSE_FILEDLG
+	 void OnOpen(wxCommandEvent& event);
+#endif // wxUSE_FILEDLG
+
+
 
 private:
 
+	void RecreateAnimation();
+	wxAnimationCtrlBase* m_animationCtrl;
 	wxDECLARE_EVENT_TABLE();
 };
 
